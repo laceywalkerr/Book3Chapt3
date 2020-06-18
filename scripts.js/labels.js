52448bba4b33e85182aacbb584d8eb27044ba997
Lacey Walker <65372269+laceywalkerr@users.noreply.github.com>
2020-06-17 23:18:25 -0500

    added artists and labels

---
+++ scripts.js/labels.js
@@ -0,0 +1,12 @@
+var jumpStop = ["funk", "rap"]
+var chatten = ["country", "blueGrass"]
+var polar = ["pop"]
+
+const createBlueGrass = function (artistName, artistAge) {
+    return {
+        "artistName": artistName,
+        "artistAge": artistAge
+    }
+}
+
+export default createBlueGrass
